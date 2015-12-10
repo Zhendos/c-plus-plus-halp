220bf6f303aa1047cd2c5fc8679551148fe83158
Youri <youri_da_bomb@hotmail.com>
2015-12-10 17:45:10 +0100

    Create output.cpp

---
+++ output.cpp
@@ -0,0 +1,22 @@
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+[SYSTEM] Player object created
+1
+5
+6
+8
+6
+7
+3
+11
+0
+-858993460
+-858993460
